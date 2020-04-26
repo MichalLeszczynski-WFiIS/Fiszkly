15c09d5370f5fcf648f413607cca50d2f0fe0396
tomray <tomraydev@gmail.com>
2020-04-26 20:44:42 +0200

    format files with black

--- fiszkly/settings/production.py
+++ fiszkly/settings/production.py
@@ -2,7 +2,7 @@ from fiszkly.settings.common import *
 
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 DEBUG = False
-ALLOWED_HOSTS = ['77.55.214.25']
+ALLOWED_HOSTS = ["77.55.214.25"]
 SECURE_REFERRER_POLICY = "no-referrer"
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
