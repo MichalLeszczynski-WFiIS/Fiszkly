18e0da37f3033b3821a2f15deb5ae66aff3ea7b3
Stanisław Tęczyński <stanislaw.teczynski@gmail.com>
2020-04-27 21:31:48 +0200

    checkpoint 1

--- fiszkly/settings/common.py
+++ fiszkly/settings/common.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "learning.apps.LearningConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -45,7 +46,7 @@ ROOT_URLCONF = "fiszkly.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- fiszkly/urls.py
+++ fiszkly/urls.py
@@ -16,4 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("learning.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("learning/", include("learning.urls")),
+    path('', include('accounts.urls'))
+
+
+]
