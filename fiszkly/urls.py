267971ba50a6051e2a7fc71b3da87f115c4edaea
Stanisław Tęczyński <stanislaw.teczynski@gmail.com>
2020-04-27 21:55:37 +0200

    formatted files with black #2

--- fiszkly/urls.py
+++ fiszkly/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("learning/", include("learning.urls")),
-    path('', include('accounts.urls'))
+    path("", include("accounts.urls")),
 ]
