bba2840344586944c8e7902e44feac21fa431b56
Stanisław Tęczyński <stanislaw.teczynski@gmail.com>
2020-04-30 18:25:00 +0200

    version 1

--- fiszkly/settings/common.py
+++ fiszkly/settings/common.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "learning.apps.LearningConfig",
+    "accounts.apps.AccountsConfig",
     "frontend",
 ]
 
