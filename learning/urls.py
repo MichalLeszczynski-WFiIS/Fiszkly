ca58793f0d294c92c99558a07c759a5f3f19b637
dtrybuch <dominik.trybuch@gmail.com>
2020-05-07 20:19:38 +0200

    Add flashcard main functionality

--- learning/urls.py
+++ learning/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from learning import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [path("", views.index, name="index"),
+               path("test/<int:id>", views.learning, name="test"),
+               path("save_answer/", views.save_answer)]
--- learning/views.py
+++ learning/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse, redirect
+# from django.views.decorators.http import require_http_methods, require_POST
+import json
 
 def index(request):
     user = None
@@ -7,3 +8,41 @@ def index(request):
         user = request.user
     context = {"user": user}
     return render(request, "index.html", context)
+
+def learning(request, id):
+    if request.user.is_authenticated:
+        word = {
+            'id':1,
+            'content':'miły'
+        }
+        answers = {
+            '1':'nice',
+            '2':'awful',
+            '3':'terrible'
+        }
+        context = {
+            "word": word,
+            'answers':answers
+        }
+        return render(request, "learning.html", context)
+    else:
+        return redirect("/")
+
+def save_answer(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            print(request.user)
+            next_word_id = 1 #here we get id from source
+            
+            id = request.POST["id"]
+            print(id == 1)
+            if id == '1':
+                data = json.dumps({"next_word_id": next_word_id, "is_correct": True})
+                print(data)
+                return HttpResponse(data)
+            else:
+                data = json.dumps({"next_word_id": next_word_id, "is_correct": False})
+                print(data)
+                return HttpResponse(data)
+    else:
+        return HttpResponse(status=401)
