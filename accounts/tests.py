1e8c093e149b5446bd7557136a66c454d19934ed
Michał Leszczyński <37879532+MichalLeszczynski@users.noreply.github.com>
2020-05-12 09:10:41 +0200

    Add login test (#23)

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+
+class LogInTest(TestCase):
+    def setUp(self):
+        self.credentials = {"username": "testuser", "password": "secret"}
+        User.objects.create_user(
+            username=self.credentials.get("username"), password=self.credentials.get("password")
+        )
+
+    def test_login(self):
+        response = self.client.post("/login/", self.credentials, follow=True)
+        self.assertTrue(response.context["user"].is_authenticated)
