9446d670408e6e70ca93eaca5c9e5d66c9909278
Anna Malik <annanmalik@hotmail.com>
2020-05-12 16:53:16 +0200

    main nav

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -1,60 +1,56 @@
+import React from 'react';
 import {render} from "react-dom";
-import React, {useEffect, useState} from "react";
-import styled from 'styled-components'
+
+import Paper from "@material-ui/core/Paper";
+import MenuList from "@material-ui/core/MenuList";
+import MenuItem from "@material-ui/core/MenuItem";
+import { withStyles } from '@material-ui/core/styles';
+
+
+const StyledMenu = withStyles({
+    root: {
+        height: "600px",
+        width: "250px",
+        background: "rgba(255, 255, 255, .8)",
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+})(MenuList);
+
 
 const Menu = () => {
-    const linksArray =
-        [
-        {name: "let's learn", url: "#"},
-        {name: "ranking", url: "#"},
-        {name: "asd", url: "#"},
-        {name: "settings", url: "#"}
-    ]
 
-    console.log("asasdasd")
+    return (
+        <div>
+        <Paper>
+            <StyledMenu>
 
-        return (
-            <div>
+                <MenuItem onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = 'http://localhost:8000/';
+                }}> Learning</MenuItem>
 
-                <Links links={linksArray} />
-            </div>
-        );
-}
+                <MenuItem onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = 'http://localhost:8000/register';
+                }}> My account </MenuItem>
 
-const Links = (props) => {
-    console.log("asasdasd links")
-        props.links.map((link) => (
-            <li className="link">
-                <a href={link.url}>{link.name}</a>
-            </li>
-            ));
-    }
+                <MenuItem onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = 'http://localhost:8000/register';
+                }} > Ranking </MenuItem>
 
+                <MenuItem onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = 'http://localhost:8000/logout';
+                }} > Logout </MenuItem>
 
-export default Menu;
+            </StyledMenu>
+        </Paper>
+        </div>
+
+    )}
+
+export default (Menu);
 
 const container = document.getElementById("menu");
 render(<Menu />, container);
-
-const LinkList = styled.ul`
-  padding-top: 90px;
-  padding-right: 25px;
-  padding-left: 25px;
-`
-
-const Panel = styled.div`
-  height: 600px;
-  width: 250px;
-  background-color: rgba(255, 255, 255, .8);
-  margin-left: -450px;
-  margin-top: -10px;
-  opacity: 0;
-  -webkit-box-shadow: 10px 10px 6px -8px rgba(161, 136, 119, .7);
-  transition: all .3s ease-in-out;
-  border-radius: 2px;
-  padding: 0 auto;
-  border-right: 1px solid rgba(0, 0, 0, .2);
-  border-bottom: 1px solid rgba(0, 0, 0, .2);
-  margin-left: 0px;
-  opacity: 1;
-`
