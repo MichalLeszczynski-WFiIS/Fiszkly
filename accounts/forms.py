3dc6f3dad1b636a38d8f23f78adb4c803cb3a893
Anna Malik <annanmalik@hotmail.com>
2020-05-15 09:37:55 +0200

    add frontend.md

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import User
 
 class CreateUserForm(UserCreationForm):
     password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={"placeholder": "Password"}), required=True
+        widget=forms.PasswordInput(attrs={"placeholder": ""}), required=True
     )
     password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}), required=True,
+        widget=forms.PasswordInput(attrs={"placeholder": ""}), required=True,
     )
 
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
         widgets = {
-            "username": forms.fields.TextInput(attrs={"placeholder": "Username"}),
-            "email": forms.fields.TextInput(attrs={"placeholder": "Email"}),
+            "username": forms.fields.TextInput(attrs={"placeholder": ""}),
+            "email": forms.fields.TextInput(attrs={"placeholder": ""}),
         }
