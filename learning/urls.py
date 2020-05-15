dfc2006b34276326daad18be64daec4ffa3e0fed
MichalLeszczynski <mleszczynsk@gmail.com>
2020-05-15 15:58:06 +0200

    Black

--- learning/urls.py
+++ learning/urls.py
@@ -3,9 +3,7 @@ from learning import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path(
-        "check_user_skills/<slug:id>", views.check_user_skills, name="check_user_skills"
-    ),
+    path("check_user_skills/<slug:id>", views.check_user_skills, name="check_user_skills"),
     path("save_answer/", views.save_answer),
     path("get_answer/", views.get_answer),
 ]
