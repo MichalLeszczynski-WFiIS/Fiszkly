a3b11b64f49f4bf11bebcef20e447f01c242076b
Anna Malik <annanmalik@hotmail.com>
2020-05-15 13:26:22 +0200

    change urls.py

--- fiszkly/urls.py
+++ fiszkly/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("learning.urls")),
     path("", include("accounts.urls")),
+    path("", lambda request: redirect('learning/', permanent=True))
 ]
