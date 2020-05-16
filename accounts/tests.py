c525e12b3be32cbc8bebb656337ceb5a55e8671f
MichalLeszczynski <mleszczynsk@gmail.com>
2020-05-16 15:37:12 +0200

    Add login-related tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -13,4 +13,19 @@ class LogInTest(TestCase):
 
     def test_login(self):
         response = self.client.post("/login/", self.credentials, follow=True)
+        self.assertEquals(response.status_code, 200)
         self.assertTrue(response.context["user"].is_authenticated)
+
+    def test_login_with_bad_password(self):
+        response = self.client.post(
+            "/login/", {"username": "testuser", "password": "credential"}, follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(response.context["user"].is_authenticated)
+
+    def test_login_with_bad_username(self):
+        response = self.client.post(
+            "/login/", {"username": "bad", "password": "credential"}, follow=True
+        )
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(response.context["user"].is_authenticated)
