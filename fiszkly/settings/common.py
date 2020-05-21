9f73724dd79fdd093e32acf4990d6ebb054e3868
dtrybuch <dominik.trybuch@gmail.com>
2020-05-21 20:48:02 +0200

    restore changes wirh email notifications

--- fiszkly/settings/common.py
+++ fiszkly/settings/common.py
@@ -81,12 +81,13 @@ CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
 CELERY_TIMEZONE = "Europe/Warsaw"
 CELERY_BEAT_SCHEDULE = {}
-CELERY_IMPORTS = ("accounts.tasks",)
+CELERY_IMPORTS = ("learning.tasks",)
 
 CELERY_BEAT_SCHEDULE = {
     "task-number-one": {
-        "task": "accounts.tasks.send_email_notifications",
-        "schedule": crontab(minute=0, hour=9),
+        "task": "learning.tasks.send_email_notifications",
+        "schedule": crontab(minute=59, hour=23),
+        "schedule": timedelta(seconds=10),
     }
 }
 # Database
