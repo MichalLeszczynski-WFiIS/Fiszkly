c09a0f6a0c83e26fc09b84eb4134cbee6b5e05be
MichalLeszczynski <mleszczynsk@gmail.com>
2020-06-07 18:28:19 +0200

    Add words tests

--- words/tests.py
+++ words/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from words.utils import Translator, MockTranslator, get_dictionary_entry, save_flashcard
+from words.models import Flashcard
 
-# Create your tests here.
+
+class MockTranslatorTest(TestCase):
+    def setUp(self):
+        self.translator = MockTranslator("mock")
+
+    def test_mock_translate(self):
+        words = self.translator.translate(["test1", "test2"], "en", "pl")
+        self.assertIsNotNone(words)
+        for word in words:
+            self.assertEqual(word["translation"], f"t_{word['original']}")
+
+
+class DictionaryEntryTest(TestCase):
+    def test_dictionary_entry(self):
+        entry = get_dictionary_entry("dog")[0]["meaning"]
+        self.assertIn("noun", entry)
+        self.assertIn("transitive verb", entry)
+
+
+class SaveFlashcardTest(TestCase):
+    def test_save_flashcard(self):
+        word = {
+            "original": "test",
+            "translation": "t_test",
+            "sl": "en",
+            "tl": "pl",
+            "dictionary_entry": r"[example_dictionary_entry]",
+            "author": None,
+        }
+        save_flashcard(word)
+        flashcard = Flashcard.objects.get(original_word="test")
+        self.assertEqual(flashcard.translated_word, "t_test")
+        self.assertEqual(flashcard.original_language, "en")
+        self.assertEqual(flashcard.translated_language, "pl")
+        self.assertEqual(flashcard.author, None)
