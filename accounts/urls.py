a9ab57f38cb5beaa451aa3dd7b5cace8540d0f28
estewui <48382351+estewui@users.noreply.github.com>
2020-06-12 07:59:37 +0200

    Add cross-user ranking view (#43)
    * ranking view checkpoint 1
    
    * checkpoint 2 of ranking view
    
    * formatted files with black
    
    * removed unused print

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", views.login_page, name="login"),
     path("logout/", views.logout_user, name="logout"),
     path("profile/", views.profile_page, name="profile"),
+    path("statistics/", views.statistics_page, name="statistics"),
 ]
