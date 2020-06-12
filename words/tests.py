00631d6ff3db542b54982a1061f5be21b3570cb2
MichalLeszczynski <mleszczynsk@gmail.com>
2020-06-12 08:55:15 +0200

    Add test: adding categorized flashcards

--- words/tests.py
+++ words/tests.py
@@ -1,6 +1,12 @@
 from django.test import TestCase
-from words.utils import Translator, MockTranslator, get_dictionary_entry, save_flashcard
-from words.models import Flashcard
+from words.utils import (
+    Translator,
+    MockTranslator,
+    get_dictionary_entry,
+    save_flashcard,
+    save_categorized_flashcard,
+)
+from words.models import Flashcard, FlashcardGroup
 
 
 class MockTranslatorTest(TestCase):
@@ -37,3 +43,32 @@ class SaveFlashcardTest(TestCase):
         self.assertEqual(flashcard.original_language, "en")
         self.assertEqual(flashcard.translated_language, "pl")
         self.assertEqual(flashcard.author, None)
+
+    def test_save_categorized_flashcard(self):
+        word = {
+            "original": "test",
+            "translation": "t_test",
+            "sl": "en",
+            "tl": "pl",
+            "dictionary_entry": r"[example_dictionary_entry]",
+            "author": None,
+        }
+        category = FlashcardGroup(name="TestCategory")
+        category.save()
+        save_categorized_flashcard(word, category)
+
+        flashcard = Flashcard.objects.filter(flashcardgroup__name="TestCategory")[0]
+        self.assertEqual(flashcard.translated_word, "t_test")
+        self.assertEqual(flashcard.original_language, "en")
+        self.assertEqual(flashcard.translated_language, "pl")
+        self.assertEqual(flashcard.author, None)
+
+
+class BrowseWordsViewTest(TestCase):
+    def test_browse_words(self):
+        pass
+
+
+class VerifyWordsViewTest(TestCase):
+    def test_verify_words(self):
+        pass
